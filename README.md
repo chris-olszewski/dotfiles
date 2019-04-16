f41eb661cc9eeba74052dde81979172a6681cade
Chris Olszewski <chrisdolszewski@gmail.com>
2019-04-16 13:07:12 -0500

    add doom snippets, add sane rust mode bindings

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # dotfiles
 Ideally should be used for both macOS and Arch installs.
-
+Includes configuration for XMonad, Doom Emacs, Bash & misc. software.
--- doom/config.el
+++ doom/config.el
@@ -5,3 +5,15 @@
 
 (setq doom-font (font-spec :family "Fira Mono" :size 12))
 (setq evil-escape-key-sequence "fd")
+
+;; Remove b prefix for default cargo bindings
+(after! rust-mode
+  (map! :map rust-mode-map
+        :localleader
+        :desc "cargo build" "b" (λ! (compile "cargo build --color always"))
+        :desc "cargo check" "c" (λ! (compile "cargo check --color always"))
+        :desc "cargo run" "r" (λ! (compile "cargo run --color always"))
+        :desc "cargo test" "t" (λ! (compile "cargo test --color always"))
+        :desc "cargo fmt" "f" (λ! (compile "cargo fmt"))
+        :desc "cargo bench" "B" (λ! (compile "cargo bench"))
+        :desc "cargo clippy" "C" (λ! (compile "cargo clippy"))))
