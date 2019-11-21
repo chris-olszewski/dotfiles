64fcd72cc3cef87f24f8a37084c8c8571b11d38c
Chris Olszewski <chrisdolszewski@gmail.com>
2019-11-21 00:33:44 -0600

    switch to dante, use vi tmux bindings, and more
    Also upgrade global stack resolver,
    Deal with xmonad deprecations

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,23 +6,33 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.SpawnOn
 
 -- TODO: add binding to put a window into a "following" state
---   	 i.e. a window that switches workspaces when user switches workspace
-borderDelta :: Int
+-- i.e. a window that switches workspaces when user switches workspace
+borderDelta :: Integer
 borderDelta = 3
 
+screenBorder :: Border
+screenBorder = Border 0 0 0 0
+
+windowBorder :: Border
+windowBorder = Border 15 15 15 15
+
+initialSpacing = spacingRaw True Main.screenBorder False Main.windowBorder True
+
+customKeys :: [(String, X ())]
 customKeys = [ ("M-[", prevWS)
              , ("M-]", nextWS)
-             , ("M-S-=", incSpacing borderDelta)
-             , ("M--", incSpacing (-borderDelta))
+             , ("M-S-=", incScreenWindowSpacing borderDelta)
+             , ("M--", incScreenWindowSpacing (-borderDelta))
              , ("M-p", spawn "dmenu_run -nf '#eceff4' -nb '#5e81ac' -sb '#8fbcbb' -sf '#2e3440'")
              ]
 
 -- look at withFocused :: (Window -> X ()) -> X ()
 -- to set a window in this state
+-- setWMState to set the window state
 
+myLayoutHook = initialSpacing $ layoutHook desktopConfig
 
-myLayoutHook = spacing 15 $ layoutHook desktopConfig
-
+main :: IO ()
 main = xmonad $ desktopConfig
        { terminal = "alacritty"
        , modMask = mod4Mask
