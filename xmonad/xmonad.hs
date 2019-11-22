9ba68f4ef4191274c617f6e5b79e82f9a6ae6683
Chris Olszewski <chrisdolszewski@gmail.com>
2019-11-21 22:37:50 -0600

    Update alacritty config, alter xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,13 @@
+
+
 import XMonad
-import XMonad.Config.Desktop
-import XMonad.Layout.Spacing
+
+import XMonad.Config.Desktop (desktopConfig)
+import XMonad.Layout.Spacing hiding (screenBorder, windowBorder)
 import XMonad.Util.EZConfig
 import XMonad.Actions.CycleWS
-import XMonad.Actions.SpawnOn
+
+--import XMonad.Actions.SpawnOn
 
 -- TODO: add binding to put a window into a "following" state
 -- i.e. a window that switches workspaces when user switches workspace
@@ -16,7 +20,7 @@ screenBorder = Border 0 0 0 0
 windowBorder :: Border
 windowBorder = Border 15 15 15 15
 
-initialSpacing = spacingRaw True Main.screenBorder False Main.windowBorder True
+initialSpacing = spacingRaw False screenBorder True windowBorder True
 
 customKeys :: [(String, X ())]
 customKeys = [ ("M-[", prevWS)
@@ -24,13 +28,13 @@ customKeys = [ ("M-[", prevWS)
              , ("M-S-=", incScreenWindowSpacing borderDelta)
              , ("M--", incScreenWindowSpacing (-borderDelta))
              , ("M-p", spawn "dmenu_run -nf '#eceff4' -nb '#5e81ac' -sb '#8fbcbb' -sf '#2e3440'")
+             , ("M-s", spawn "alacritty -e alsamixer")
              ]
 
--- look at withFocused :: (Window -> X ()) -> X ()
--- to set a window in this state
--- setWMState to set the window state
-
 myLayoutHook = initialSpacing $ layoutHook desktopConfig
+
+myManageHook :: ManageHook
+myManageHook = undefined
 
 main :: IO ()
 main = xmonad $ desktopConfig
